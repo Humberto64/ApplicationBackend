aaa638200ff9d438ad12ba30cc2d981ab3d7de8a
Humberto <humbertoescobarmoreno@gmail.com>
2025-11-19 08:37:47 -0400

    pom.xml

--- src/test/java/com/app/emsx/EmsxApplicationTests.java
+++ src/test/java/com/app/emsx/EmsxApplicationTests.java
@@ -1,13 +1,12 @@
-package com.app.emsx;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EmsxApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.app.emsx;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EmsxApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//}
